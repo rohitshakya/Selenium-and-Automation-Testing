abd14d718d1b029062b5abcdbe735ce47e7e9ca5
Rohit Shakya <rohit.rkshakya@gmail.com>
2021-02-28 13:54:31 +0530

    Delete app directory

--- app/Libraries/Excel.php
+++
@@ -1,8 +0,0 @@
-<?php 
-require_once "PHPExcel.php";
-class Excel extends PHPExcel {
-    public function __construct() {
-        parent::__construct();
-    }
-}
-?>
--- app/Views/publicview - Copy/user-dashboard.php
+++
@@ -1,5 +0,0 @@
-<?php include("header.php"); ?>
-
-hello
-
-<?php include("footer.php"); ?>
